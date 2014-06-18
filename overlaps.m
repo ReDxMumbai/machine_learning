30acb494900f423b08d033f19b0f52bf15699259
Sujeath Pareddy <p.sujeath@gmail.com>
2014-06-18 18:29:20 +0530

    fixed bug in overlap

--- overlaps.m
+++ overlaps.m
@@ -1,9 +1,9 @@
 function [yn] = overlaps(a,b,c,d)
 %UNTITLED9 Summary of this function goes here
-%   Detailed explanation goes here
-if c<b && a<c
+%   a c b d or c a d b
+if c<=b && a<=c
    yn=1;
-elseif c<b && b<d
+elseif c<=a && a<=d
    yn=1; 
 else
    yn=0;
